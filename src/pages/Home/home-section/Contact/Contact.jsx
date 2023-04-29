0bda51bacc1f8b0560108d3e93bda06658dac159
vuhai2310 <lvhai.19it4@vku.udn.vn>
2023-04-30 00:39:20 +0700

    13 - Add Effect Contact Page ✨✨

--- src/pages/Home/home-section/Contact/Contact.jsx
+++ src/pages/Home/home-section/Contact/Contact.jsx
@@ -23,62 +23,60 @@ const Contact = (props) => {
 
   return (
     <>
-      <SectionWrapper className={`${props.isActive ? "active" : ""}`}>
-        <div className="contact-section" id="contact">
-          <div className="contact-container">
-            <div className="contact-content-left">
-              <div className="contact-header">
-                <p className="contact-subtitle">Get in touch</p>
-                <h3 className="contact-title">Contact Me.</h3>
-              </div>
-              <form
-                ref={formRef}
-                onSubmit={handleSubmit}
-                className="contact-form"
-              >
-                <label className="contact-form__label">
-                  <span className="contact-form__label--text">Your Name</span>
-                  <input
-                    type="text"
-                    name="name"
-                    value={form.name}
-                    onChange={handleChange}
-                    placeholder="What's your name?"
-                    className="contact-form__label--field"
-                  />
-                </label>
-                <label className="contact-form__label">
-                  <span className="contact-form__label--text">Your Email</span>
-                  <input
-                    type="email"
-                    name="email"
-                    value={form.email}
-                    onChange={handleChange}
-                    placeholder="What's your email?"
-                    className="contact-form__label--field"
-                  />
-                </label>
-                <label className="contact-form__label">
-                  <span className="contact-form__label--text">
-                    Your Message
-                  </span>
-                  <textarea
-                    rows="6"
-                    name="message"
-                    value={form.message}
-                    onChange={handleChange}
-                    placeholder="What do you want to say?"
-                    className="contact-form__label--field"
-                  />
-                </label>
-                <button className="btn btn-submit" type="submit">
-                  {loading ? "Sending..." : "Send"}
-                </button>
-              </form>
+      <SectionWrapper
+        className={`contact-section ${props.isActive ? "active" : ""}`}
+      >
+        <div className="contact-container">
+          <div className="contact-content-left">
+            <div className="contact-header">
+              <p className="contact-subtitle">Get in touch</p>
+              <h3 className="contact-title">Contact Me.</h3>
             </div>
-            <div className="contact-content-right">
-              <EarthCanvas />
-            </div>
+            <form
+              ref={formRef}
+              onSubmit={handleSubmit}
+              className="contact-form"
+            >
+              <label className="contact-form__label">
+                <span className="contact-form__label--text">Your Name</span>
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+                  placeholder="What's your name?"
+                  className="contact-form__label--field"
+                />
+              </label>
+              <label className="contact-form__label">
+                <span className="contact-form__label--text">Your Email</span>
+                <input
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  placeholder="What's your email?"
+                  className="contact-form__label--field"
+                />
+              </label>
+              <label className="contact-form__label">
+                <span className="contact-form__label--text">Your Message</span>
+                <textarea
+                  rows="6"
+                  name="message"
+                  value={form.message}
+                  onChange={handleChange}
+                  placeholder="What do you want to say?"
+                  className="contact-form__label--field"
+                />
+              </label>
+              <button className="btn btn-submit" type="submit">
+                {loading ? "Sending..." : "Send"}
+              </button>
+            </form>
+          </div>
+          <div className="contact-content-right">
+            <EarthCanvas />
           </div>
         </div>
       </SectionWrapper>
